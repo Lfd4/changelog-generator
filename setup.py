45611badcfc78e177906120058cdcc5529aac756
Lennart Suwe <lennart.suwe@lfda.de>
2020-03-13 09:48:55 +0100

    update PyPI package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
    name='python-changelog',
-   version='0.1.7',
+   version='0.2.0',
    description='A module that generates changelogs based on git tags.',
    long_description=long_description,
    long_description_content_type="text/markdown",
