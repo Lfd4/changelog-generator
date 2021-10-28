a8bf390da527833db8f72961f0001dde13a63a84
Lennart Suwe <lennart.suwe@lfda.de>
2021-10-28 12:56:42 +0200

    chore: bump package version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     
 setup(
    name='python-changelog',
-   version='0.3.1',
+   version='0.3.2',
    description='A module that generates changelogs based on git tags.',
    long_description=long_description,
    long_description_content_type="text/markdown",
